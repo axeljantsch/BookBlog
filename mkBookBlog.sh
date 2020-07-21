cc61337390a8fb1b0336ec271aa38ba772f183db
Axel Jantsch <axel.jantsch@tuwien.ac.at>
2020-07-21 07:06:42 +0200

    AJ: Debugging the script

--- mkBookBlog.sh
+++ mkBookBlog.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# For debugging:
+set -x
+
+bundle -v
+jekyll -v
+bundle exec jekyll -v
+# End debugging
+
 srcDir=$HOME/developments/WebPages/BookBlog-Jekyll-4.0/Github
 buildDir=$srcDir/_site
 targetDir=$HOME/Website/jantsch.se/BookBlog
